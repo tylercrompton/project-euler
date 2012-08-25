80729973deb6280111a605b4259b5158ba4fdddb
Tyler Crompton <tyler@tylercrompton.com>
2012-08-24 19:39:10 -0500

    added Python implementation of p2

---
+++ p2.py
@@ -0,0 +1,22 @@
+import sys as _sys
+
+def even_fib_sum(upper_bound):
+	if upper_bound <= 2: return 0
+
+	buffer = [0, 2]
+
+	sum = 2
+	temp = 8
+	while temp < upper_bound:
+		buffer[0], buffer[1] = buffer[1], temp
+		sum += temp
+		temp = 4 * buffer[1] + buffer[0]
+
+	return sum
+
+if __name__ == '__main__':
+	if len(_sys.argv) == 2:
+		print(even_fib_sum(int(_sys.argv[1])))
+	else:
+		print('Usage: p2 <integer>')
+		_sys.exit(1)
