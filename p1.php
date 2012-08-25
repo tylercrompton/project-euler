d2191dccf877d2ac1f1645ed04fd902e22f0713c
Tyler Crompton <tyler@tylercrompton.com>
2012-08-25 10:15:18 -0500

    Added PHP implementation of p1

---
+++ p1.php
@@ -0,0 +1,33 @@
+<?php
+
+function p1($max) {
+	$i = 0;
+	$total = 0;
+
+	while ($i < $max) {
+		if ($i + 15 < $max)
+			$total += $i * 7 + 60;
+		else if ($i + 12 < $max)
+			$total += $i * 6 + 45;
+		else if ($i + 10 < $max)
+			$total += $i * 5 + 33;
+		else if ($i + 9 < $max)
+			$total += $i * 4 + 23;
+		else if ($i + 6 < $max)
+			$total += $i * 3 + 14;
+		else if ($i + 5 < $max)
+			$total += $i * 2 + 8;
+		else if ($i + 3 < $max)
+			return $total + $i + 3;
+		$i += 15;
+	}
+
+	return $total;
+}
+
+if (count($argv) == 2) {
+	echo p1(intval($argv[1])), "\n";
+} else {
+	echo "Usage: p1 <int>\n";
+	exit(1);
+}
