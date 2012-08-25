382a7f7e18a73c0ce07efd019c1a5d5d5016ce83
Tyler Crompton <tyler@tylercrompton.com>
2012-08-24 19:30:34 -0500

    added Java implementation of p2

---
+++ p2.java
@@ -0,0 +1,23 @@
+public class p2 {
+	public static int even_fib_sum(int upper_bound) {
+		if (upper_bound <= 2) return 0;
+
+		int buffer[] = new int[] {0, 2},
+			sum = 2,
+			temp = 8;
+
+		while (temp < upper_bound) {
+			buffer[0] = buffer[1];
+			buffer[1] = temp;
+			sum += temp;
+			temp = 4 * buffer[1] + buffer[0];
+		}
+
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		if (args.length == 1) System.out.println(even_fib_sum(Integer.parseInt(args[0])));
+		else System.out.println("Usage: p2 <integer>");
+	}
+}
